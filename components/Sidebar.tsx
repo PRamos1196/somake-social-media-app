5ed4c354819ef6fe0a8f9a320ea54d8680d37735
PRamoss1196 <pramos1996@Outlook.com>
2023-01-13 12:21:20 -0500

    Navbar and sidebar have been worked on, not complete need to do more components for the rest will continue in other branches

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {NextPage } from 'next';
+import Link from 'next/link';
+
+import { useRouter } from 'next/router';``
+import GoogleLogin from 'react-google-login';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
+
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
+
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+
+    const [showSidebar, setShowSidebar] = useState(true);
+
+    const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#70C8E8] rounded';
+
+    const userProfile = false;
+
+    return (
+        <div>
+            <div 
+                className="block xl:hidden m-2 ml-4 mt-3 text-xl text-[#70C8E8]"
+                onClick={() => setShowSidebar ((prev) => !prev)}
+            >
+                {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+            </div>
+            {showSidebar && (
+                <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+                    <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+                        <Link href="/">
+                            <div className={normalLink}>
+                                <p className="text-2xl">
+                                    <AiFillHome />
+                                </p>
+                                <span className="text-xl hidden xl:block">
+                                    For You
+                                </span>
+                            </div>
+                        </Link>
+                    </div>
+                    {!userProfile && (
+                        <div className="px-2 py-4 hidden xl:block">
+                            <p className="text-gray-400">Log In to Like and Comment on Videos</p>
+                            <div className="pr-4">
+                                <GoogleLogin
+                                    render={(renderProps)  => (
+                                        <button
+                                        className="bg-white text-lg text-[#70C8E8] border-[1px] border-[#70C8E8] 
+                                            font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#70C8E8]
+                                            cursor-pointer"
+                                            onClick={renderProps.onClick}
+                                            disabled={renderProps.disabled}
+                                        >
+                                            Log In
+                                        </button>
+                                    )}
+                                    clientId=''
+                                    onSuccess={() => {}}
+                                    onFailure={() => {}}
+                                    cookiePolicy='single_host-origin'
+                                />
+                            </div>
+                        </div>
+                    )}
+                    <Discover />
+                    <SuggestedAccounts />
+                    <Footer />
+                </div>
+                
+            )}
+        </div>
+    )
 }
 
 export default Sidebar
