94f3e62ff6b298c2436f212ff45b05fef3bbcd13
PRamoss1196 <pramos1996@Outlook.com>
2023-01-15 10:37:48 -0500

    Google OAuthentication hasd been made and finished.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
@@ -13,6 +14,7 @@ import Logo from '../utils/Somake-logo.png';
 
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
         <Link href="/">
@@ -20,6 +22,21 @@ const Navbar = () => {
                 <Image className="cursor-pointer" src={Logo} alt="Somake" layout="responsive"/>
             </div>
         </Link>
+        <div>
+          SEARCH
+        </div>
+        <div>
+          {user ? (
+            <div> Logged In</div>
+          ) : (
+            <GoogleLogin 
+              onSuccess={(response) => console.
+                log(response)}
+              onError={() => console.
+                log('error')}
+            />
+          )}
+        </div>
     </div>
   )
 }
