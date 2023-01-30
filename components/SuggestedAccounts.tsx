839fdd14bfe569ede5e7944b4bcacc1e4cfb7f8f
PRamoss1196 <pramos1996@Outlook.com>
2023-01-30 12:31:15 -0500

    Still working on comments, almost done, have sugegsted accounts recommended as well in splices of 6 people it doesnt take an algorithm it genuinely is random based on userID

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { GoVerified } from 'react-icons/go';
+
+import useAuthStore from '../store/authStore';
+import { IUser } from '../types';
 
 const SuggestedAccounts = () => {
+
+  const { fetchAllUsers, allUsers } = useAuthStore();
+  useEffect(() => {
+    fetchAllUsers();
+  }, [fetchAllUsers]);
+  
   return (
-    <div className="text-white">SuggestedAccounts</div>
+    <div className="text-white xl:border-b-2 border-gray-100 pb-4">
+      <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
+        Suggest Accounts
+      </p>
+      <div>
+        {allUsers.slice(0, 6).map((user: IUser) =>(
+         <Link key={user._id} href={`/profile/${user._id}`}>
+          <div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded">
+            <div className="w-8 h-8">
+              <Image 
+                  src={user.image}
+                  width={34}
+                  height={34} 
+                  className="rounded"
+                  alt="user profile"
+              />
+            </div>
+            <div className="hidden xl:block">
+              <p className="flex gap-1 items-center text-md font-bold text-white">
+                {user.userName.replaceAll(' ', '')}
+                <GoVerified className="text-blue-400"/>
+              </p>
+              <p className="capitalize text-gray-400 text-xs">
+                {user.userName}
+              </p>
+            </div>
+          </div>
+         </Link>
+        ))}
+      </div> 
+    </div>
   )
 }
 
--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { postDetailQuery } from '../../../utils/queries';
 import { client } from '../../../utils/client';
+import { v4 as uuidv4 } from 'uuid';
 
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
@@ -8,7 +9,25 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
        const { id } = req.query;
        const query = postDetailQuery(id!);
        const data = await client.fetch(query);
+       const { v4: uuidv4 } = require('uuid');
 
        res.status(200).json(data[0]);
+    } else if (req.method === 'PUT') {
+        const { comment, userId } = req.body;
+        const { id }: any = req.query;
+
+        const data =  await client
+          .patch(id)
+          .setIfMissing({ comments: [] })
+          .insert('after', 'comments[-1]', [
+            {
+                comment,
+              _key: uuidv4(),
+              postedBy: { _type: 'postedBy', _ref: userId }
+            },
+          ])
+          .commit()
+
+          res.status(200).json(data);
     }
 }
