f0c495c3e9c2e8b1ca8d53a50ab04d31779a7cad
PRamoss1196 <pramos1996@Outlook.com>
2023-02-03 23:39:15 -0500

    Changes made to Upload, Uploading speed should be improved!

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -12,7 +12,9 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
        const { v4: uuidv4 } = require('uuid');
 
        res.status(200).json(data[0]);
-    } else if (req.method === 'PUT') {
+    } 
+    else if (req.method === 'PUT') 
+    {
         const { comment, userId } = req.body;
         const { id }: any = req.query;
 
